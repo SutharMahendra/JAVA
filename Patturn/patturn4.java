e695d29d93e826de6f83a758d8cc8ed4184ae3c5
SutharMahendra <omajimahendra@gmail.com>
2024-03-11 10:57:37 +0530

    Fourth patturn

---
+++ Patturn/patturn4.java
@@ -0,0 +1,14 @@
+package Patturn;
+
+public class patturn4 {
+    public static void main(String[] args) {
+
+        for (int i = 4; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+}
