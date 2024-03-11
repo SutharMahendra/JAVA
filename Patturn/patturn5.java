aaaccc023dce00e9a22e70dc5d649ddfa25888b7
SutharMahendra <omajimahendra@gmail.com>
2024-03-11 10:59:36 +0530

    Fifth patturn

--- Patturn/patturn5.java
+++ Patturn/patturn5.java
@@ -1,19 +1,13 @@
 package Patturn;
 
-// this patturn look like .. 
-// ****
-// ***
-// **
-// *
-public class patturn4 {
+public class patturn5 {
     public static void main(String[] args) {
 
-        for (int i = 4; i > 0; i--) {
-            for (int j = i; j > 0; j--) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
     }
-
 }
