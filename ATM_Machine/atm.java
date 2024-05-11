b3e86f756022f668a480b0ab216061417b5d23ee
SutharMahendra <omajimahendra@gmail.com>
2024-05-11 22:16:38 +0530

    ATM Mini project

---
+++ ATM_Machine/atm.java
@@ -0,0 +1,11 @@
+package ATM_Machine;
+
+import java.io.IOException;
+
+public class atm extends OptionMenu {
+    public static void main(String args[]) throws IOException {
+        OptionMenu optionMenu = new OptionMenu();
+        optionMenu.getlogin();
+    }
+
+}
